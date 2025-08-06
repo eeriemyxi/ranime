42b3869a0131cb9adb4b239d62bc17b70d9df6d5
eeriemyxi <myxi@envs.net>
2025-08-06 23:24:20 +0530

    fix: provide flag name specifically

--- src/ranime/cli.py
+++ src/ranime/cli.py
@@ -16,23 +16,23 @@ from ranime import (CACHE_PATH, display_anime_info, fetch_anilist_cover_image,
 @click.option("--id", "-i", default=None, help="ID of the generated list from randomanime.org")
 @click.option("--preset-name", "-p", default=None, help="Name of the preset to save --auth-key and --id under")
 def main(auth_key: str, id: str, preset_name: str):
-    def load_or_save(name: str, value: str, tip: str):
+    def load_or_save(name: str, flag_name: str, value: str, tip: str):
         path = CACHE_PATH / name
         if not value:
             try:
                 value = path.read_text()
             except FileNotFoundError:
                 print(
-                    f"ERROR: --{name.replace('_', '-')} was not provided but {path} doesn't exist yet either.\n\nTIP: {tip}"
+                    f"ERROR: --{flag_name} was not provided but {path} doesn't exist yet either.\n\nTIP: {tip}"
                 )
                 exit(1)
         else:
             path.write_text(value)
         return value
 
-    preset_name = load_or_save("active_preset", preset_name, "Run ranime with --preset-name at least once.")
-    auth_key = load_or_save(f"{preset_name}_auth_key", auth_key, "Run ranime with --auth-key at least once.")
-    id = load_or_save(f"{preset_name}_id", id, "Run ranime with --id at least once.")
+    preset_name = load_or_save("active_preset", "preset-name", preset_name, "Run ranime with --preset-name at least once.")
+    auth_key = load_or_save(f"{preset_name}_auth_key", "auth-key", auth_key, "Run ranime with --auth-key at least once.")
+    id = load_or_save(f"{preset_name}_id", "id", id, "Run ranime with --id at least once.")
 
     page_amnt = get_randomeanime_page(auth_key=auth_key, id=id, page=1).json()[
         "resultsTotal"
